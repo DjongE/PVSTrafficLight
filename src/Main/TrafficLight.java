4e11f602082c8b8ca757144ecf0e742fa7886517
aa.zanabili <ab6025@hs-el.de>
2022-03-30 16:18:02 +0200

    fix

--- src/Main/TrafficLight.java
+++ src/Main/TrafficLight.java
@@ -1,4 +1,4 @@
-package main;
+package Main;
 
 import pds.trafficlight.CardinalDirection;
 import pds.trafficlight.Colour;
@@ -23,6 +23,10 @@ public class TrafficLight extends Thread {
   private static volatile Colour nextColor; // Next (expected) traffic light colour
 
   private static volatile boolean stopped = false;
+  private static volatile boolean mainReady = false;
+  private static volatile boolean oppReady = false;
+  
+  
 
   private static final Object lock = new Object();
 
@@ -40,6 +44,7 @@ public class TrafficLight extends Thread {
     nextColor = Colour.GREEN;
     this.cd = cd;
     TrafficLight.dir = dir;
+    
   }
 
   /**
@@ -48,30 +53,39 @@ public class TrafficLight extends Thread {
   @Override
   public void run() {
 
-    Reporter.show(cd, color);
+	  Reporter.show(cd, color);
 
     while (!stopped) {
-      synchronized (lock) { // mutual exclusion (critical area)
+      
         if (cd == dir || cd == CardinalDirection.opposite(dir)) {
-
+        	 // mutual exclusion (critical area)
           if (color != nextColor) { // If current colour is not equal to next (expected) colour
-            color = Colour.next(color); // Switch current traffic light to the next colour
-            Reporter.show(cd, color);
+        	  
+            synchronized (lock) {
+            	
             if (cd == dir) {
-              mainColor = color; // The colour of the current traffic light
-            } else {
-              oppositeColor = color; // The colour of the traffic light opposite
+              mainReady = true;
+              color = Colour.next(color); // Switch current traffic light to the next colour
+              Reporter.show(cd, color);
+            } else if(cd == CardinalDirection.opposite(dir)) {
+              oppReady = true;
+              color = Colour.next(color); // Switch current traffic light to the next colour
+              Reporter.show(cd, color);
             }
-          }
-
-          if (oppositeColor == mainColor) { // opposite color is the same as the main color
-            nextColor = Colour.next(nextColor); // Next (expected) traffic light colour
-
-            if (nextColor == Colour.GREEN) { // If expected colour is green, the Axis will switch
-              dir = CardinalDirection.next(cd);
             }
+            synchronized (lock) {
+                
+        	
+            if (mainReady && oppReady && (cd == dir || cd == CardinalDirection.opposite(dir))) { // opposite color is the same as the main color
+                nextColor = Colour.next(nextColor); // Next (expected) traffic light colour
+                mainReady = false;
+                oppReady = false;
+                if (nextColor == Colour.GREEN) { // If expected colour is green, the Axis will switch
+                  dir = CardinalDirection.next(cd);
+                }
+              }
           }
-        }
+        	}
       }
     }
   }
