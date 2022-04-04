c560b2e053c3380cdd39c56f6604a3c0165737be
aa.zanabili <ab6025@hs-el.de>
2022-04-04 17:02:28 +0200

    fertig v1.3

--- src/main/TrafficLight.java
+++ src/main/TrafficLight.java
@@ -18,15 +18,11 @@ public class TrafficLight extends Thread {
   private static volatile CardinalDirection dir;
 
   private Colour color; // Colour of the traffic light
-  private static volatile Colour mainColor; // Colour of the "main traffic light"
-  private static volatile Colour oppositeColor; // Colour of the opposite traffic light
   private static volatile Colour nextColor; // Next (expected) traffic light colour
 
   private static volatile boolean stopped = false;
-  private static volatile boolean mainReady = false;
-  private static volatile boolean oppReady = false;
-  
-  
+  private static volatile boolean mainReady = false; // Main traffic light is ready
+  private static volatile boolean oppReady = false; // Opposite traffic light is ready
 
   private static final Object lock = new Object();
 
@@ -35,17 +31,14 @@ public class TrafficLight extends Thread {
    *
    * @param cd  this is an cardinal direction of the location of this traffic light
    * @param dir this direction of the sky indicates which direction of the
-    *            traffic light is allowed to start and the opposite traffic
-    *            light is also allowed to start.
+   *            traffic light is allowed to start and the opposite traffic
+   *            light is also allowed to start.
    */
   public TrafficLight(CardinalDirection cd, CardinalDirection dir) {
     color = Colour.RED;
-    oppositeColor = Colour.RED;
     nextColor = Colour.GREEN;
     this.cd = cd;
     TrafficLight.dir = dir;
-    
-    
   }
 
   /**
@@ -54,49 +47,45 @@ public class TrafficLight extends Thread {
   @Override
   public void run() {
 
-	  Reporter.show(cd, color);
+    Reporter.show(cd, color);
 
     while (!stopped) {
-      
-        if (cd == dir || cd == CardinalDirection.opposite(dir)) {
-        	 // mutual exclusion (critical area)
-          if (color != nextColor) { // If current colour is not equal to next (expected) colour
-        	  
-            synchronized (lock) {
-            	
+      if (cd == dir || cd == CardinalDirection.opposite(dir)) {
+        if (color != nextColor) { // If current colour is not equal to next (expected) colour
+
+          synchronized (lock) { // Mutual exclusion (critical area)
             if (cd == dir) {
               mainReady = true;
               color = Colour.next(color); // Switch current traffic light to the next colour
               Reporter.show(cd, color);
-            } else if(cd == CardinalDirection.opposite(dir)) {
+            } else if (cd == CardinalDirection.opposite(dir)) {
               oppReady = true;
               color = Colour.next(color); // Switch current traffic light to the next colour
               Reporter.show(cd, color);
             }
-            }
-            synchronized (lock) {
-                
-        	
-            if (mainReady && oppReady && (cd == dir || cd == CardinalDirection.opposite(dir))) { // opposite color is the same as the main color
-                nextColor = Colour.next(nextColor); // Next (expected) traffic light colour
-                mainReady = false;
-                oppReady = false;
-                if (nextColor == Colour.GREEN) { // If expected colour is green, the Axis will switch
-                  dir = CardinalDirection.next(cd);
-                }
-              }
           }
-        	}
+
+          synchronized (lock) { // Mutual exclusion (critical area)
+            if (mainReady && oppReady) { // If main and opposite traffic light are ready
+              nextColor = Colour.next(nextColor); // Next (expected) traffic light colour
+              mainReady = false;
+              oppReady = false;
+              if (nextColor == Colour.GREEN) { // If expected colour is green, the Axis will switch
+                dir = CardinalDirection.next(cd);
+              }
+            }
+          }
+        }
       }
     }
   }
 
   /**
-  * The traffic light is called to stop.
-  * Here, only one of the 4 traffic lights needs to receive the information,
+   * The traffic light is called to stop.
+   * Here, only one of the 4 traffic lights needs to receive the information,
    * as the information is stored in a shared variable.
-  * This will turn off all the traffic lights.
-  */
+   * This will turn off all the traffic lights.
+   */
   public void halt() {
     stopped = true;
   }
